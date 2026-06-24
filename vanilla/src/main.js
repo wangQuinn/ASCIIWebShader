8848b513fafbc84d0a36cbe9714a14b731f1a74a
Amatol <tianlynnwang@gmail.com>
2026-06-23 21:16:51 -0400

    added webcam and GPU processing timer because curious

--- vanilla/src/main.js
+++ vanilla/src/main.js
@@ -1,16 +1,24 @@
 const vertexShaderSource = `#version 300 es
 in vec2 a_position; //input to a vertex shader. will recieve data from a bugger
+out vec2 v_texCoord; //Pass this to fragment shader
 void main(){
   gl_Position = vec4(a_position, 0.0, 1.0);
+  //map clip space (-1 to 1) to texture space (0 to 1)
+  vec2 texCoord = a_position * 0.5 + 0.5;
+  v_texCoord = vec2(1.0 - texCoord.x, 1.0 - texCoord.y); //flipped so that its similar to what you see irl.
 }
 `;
 const fragmentShaderSource = `#version 300 es
 precision highp float; //fragement shaders don't have a default precision so we need to pick one. highp is a good default. means "high precision".
+in vec2 v_texCoord; //received from vertex shader
 out vec4 outColor;
+
 uniform float u_time;
+uniform sampler2D u_cameraTexture;
 void main(){
-  float pulse =  sin(u_time * 0.001);
-  outColor = vec4(pulse,0,0,1); //red but from a shader this time!
+  vec4 color = texture(u_cameraTexture, v_texCoord);
+  float gray = dot(color.rgb, vec3(0.299,0.587, 0.114));
+  outColor = vec4(vec3(gray), color.a);
 }`
 
 const canvas = document.getElementById('canvas');
@@ -89,13 +97,71 @@ gl.vertexAttribPointer(
 );
 
 var timeLocation = gl.getUniformLocation(program, 'u_time');
+//GPU UI overlay, the GPU timer tells you how long it takes for you GPU to run the shaders and get an ouput. 
+let currentQuery = null;
+const statsDiv = document.getElementById('gpu-stats');
+const extTimer = gl.getExtension('EXT_disjoint_timer_query_webgl2');
+if (!extTimer) {
+  console.log('GPU Timer extension not supported on this browser/hardware.');
+  statsDiv.innerHTML = "GPU timer not supported. sorry!";
+  statsFiv.style.color = '#ff3333';
+}
+let frameCount = 0;
+
 
 function render(time){
+
+  //convert millisecond to seconds for the uniform
+  const timeInSeconds = time * 0.001;
+
   gl.viewport(0,0, canvas.width, canvas.height);
+  gl.clear(gl.COLOR_BUFFER_BIT);
+
+  //update webcamTexture
+  if(video.readyState >= video.HAVE_CURRENT_DATA){
+    gl.bindTexture(gl.TEXTURE_2D, texture);
+    gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, gl.RGBA, gl.UNSIGNED_BYTE, video);
+  }
+  
+  //GPU STUFF
+  // Process completed GPU timing queries
+  if (extTimer && currentQuery) {
+    const available = gl.getQueryParameter(currentQuery, gl.QUERY_RESULT_AVAILABLE);
+    const disjoint = gl.getParameter(extTimer.GPU_DISJOINT_EXT);
+
+    if (available && !disjoint) {
+      const timeElapsedNanos = gl.getQueryParameter(currentQuery, gl.QUERY_RESULT);
+      const timeElapsedMillis = timeElapsedNanos / 1000000;
+      
+      // Update screen text every 10 frames to avoid unreadable flickering
+      frameCount++;
+      if (frameCount % 10 === 0) {
+        statsDiv.innerHTML = `GPU Time: ${timeElapsedMillis.toFixed(3)} ms`;
+      }
+    }
+
+    if (available || disjoint) {
+      gl.deleteQuery(currentQuery);
+      currentQuery = null;
+    }
+  }
+
+  // Start timing right before drawing
+  if (extTimer && !currentQuery) {
+    currentQuery = gl.createQuery();
+    gl.beginQuery(extTimer.TIME_ELAPSED_EXT, currentQuery);
+  }
+
   gl.useProgram(program);
-  gl.uniform1f(timeLocation, time);
+  gl.uniform1f(timeLocation, time); // send time to the GPU
   gl.bindVertexArray(vao);
   gl.drawArrays(gl.TRIANGLES,0,6);
+
+  //GPU STUFF
+  if (extTimer && currentQuery) {
+    gl.endQuery(extTimer.TIME_ELAPSED_EXT);
+  }
+
   requestAnimationFrame(render);
 }
 
@@ -107,3 +173,25 @@ if(ext){
   console.log(gl.getParameter(ext.UNMASKED_VENDOR_WEBGL));
   console.log(gl.getParameter(ext.UNMASKED_RENDERER_WEBGL));
 }
+
+
+//camera
+const video = document.createElement('video');
+video.autoplay = true;
+video.playsInline = true;
+
+navigator.mediaDevices.getUserMedia({video : true}).then(stream => {
+  video.srcObject = stream;
+}).catch(err => console.error("Camera error: ", err));
+
+//web gl2 texture
+const texture = gl.createTexture();
+gl.bindTexture(gl.TEXTURE_2D, texture);
+gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.LINEAR);
+gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAX_FILTER, gl.LINEAR);
+//s and t mean x and y, clamp makes it stop at the last pixel 
+//gl_repeate makes it repeat when you go past (0,0) to (1,1)
+//gl_clamp_to_edge_ h
+gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.CLAMP_TO_EDGE);
+gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.CLAMP_TO_EDGE);
+
